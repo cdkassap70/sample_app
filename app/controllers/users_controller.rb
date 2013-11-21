5b7e03ae75c7ad1ed4f5b5e7799e9eb4cbdf79e1
cdkassap70 <KassapChristopher@gmail.com>
2013-11-20 21:56:52 -0600

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,19 @@ class UsersController < ApplicationController
   end
 
   def new
+	@user = User.new
 	@title = "Sign up"
+  end
+  
+  def create
+	@user = User.new(params[:user], :without_protection => true) #Need to find a workaround for mass assignment problem here
+	#@user = User.new(:name => params[:id]
+	if @user.save #if this returns true
+		flash[:success] = "Welcome to the Sample App!"
+		redirect_to @user
+	else
+		@title = "Sign up"
+		render 'new'
+	end
   end
 end
